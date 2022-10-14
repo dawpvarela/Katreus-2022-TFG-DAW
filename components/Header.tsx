d001be28fd49648ce8633c9c60c25d3a51bdcabb
pablo <varelapablo2002@hotmail.com>
2022-10-14 18:21:38 +0200

    Componente Link Next.js - 14/10/2022 18:17

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import { BellIcon, SearchIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 
 function Header() {
 	return (
 		<header>
 			{/* Sección izquierda del Header */}
-			<div className="">
+			<div className="flex items-center space-x-2 md:space-x-10">
 				<img
 					src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
 					width={100}
@@ -21,10 +22,17 @@ function Header() {
 			</div>
 
 			{/* Sección derecha del Header */}
-			<div>
+			<div className="flex items-center space-x-4 text-sm font-light">
 				<SearchIcon className="sm hidden h-6 w-6 sm:inline" />
 				<p className="hidden lg:inline">Kids</p>
 				<BellIcon className="h-6 w-6" />
+				<Link href="/account">
+					<img
+						src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+						alt=""
+						className="cursor-pointer rounded"
+					/>
+				</Link>
 			</div>
 		</header>
 	);
