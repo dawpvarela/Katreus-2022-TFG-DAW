4738d03c2b30ecab0733147b8c4b286b556c53ea
pablo <varelapablo2002@hotmail.com>
2022-10-15 18:18:26 +0200

    Difuminado de Fondo- 15/10/2022 18:18

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Row from '../components/Row';
