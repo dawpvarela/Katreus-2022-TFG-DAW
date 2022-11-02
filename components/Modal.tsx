e2d1a1583b634be28695e85b46394c4dacb13b2e
pablo <varelapablo2002@hotmail.com>
2022-11-03 00:20:36 +0100

    Fetch pelicula/serie trailer desde la api - 02/11/2022 00:20

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,13 +1,25 @@
-import { XIcon } from '@heroicons/react/outline';
+import {
+	CheckIcon,
+	PlusIcon,
+	ThumbUpIcon,
+	VolumeOffIcon,
+	VolumeUpIcon,
+	XIcon,
+} from '@heroicons/react/outline';
 import MuiModal from '@mui/material/Modal';
 import { useEffect, useState } from 'react';
+import { FaPlay } from 'react-icons/fa';
+import ReactPlayer from 'react-player';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { modalState } from '../atoms/modalAtom';
-import { Pelicula } from '../typings';
+import { modalState, movieState } from '../atoms/modalAtom';
+import { Elemento, Genero } from '../typings';
 
 function Modal() {
 	const [showModal, setShowModal] = useRecoilState(modalState);
-	const [pelicula, setPelicula] = useState<Pelicula | null>(null);
+	const [pelicula, setPelicula] = useRecoilState(movieState);
+	const [trailer, setTrailer] = useState('');
+	const [genres, setGenres] = useState<Genero[]>([]);
+	const [muted, setMuted] = useState(true);
 
 	useEffect(() => {
 		if (!pelicula) return;
@@ -20,15 +32,31 @@ function Modal() {
 					process.env.NEXT_PUBLIC_API_KEY
 				}&language=es-ES&append_to_response=videos`
 			).then((response) => response.json());
+
+			if (data?.videos) {
+				const index = data.videos.results.findIndex(
+					(element: Elemento) => element.type === 'Trailer'
+				);
+				setTrailer(data.videos?.results[index]?.key);
+			}
+
+			if (data?.genres) {
+				setGenres(data.genres);
+			}
 		}
-	}, []);
+		fetchMovie();
+	}, [pelicula]);
 
 	const handleClose = () => {
 		setShowModal(false);
 	};
 
 	return (
-		<MuiModal open={showModal} onClose={handleClose}>
+		<MuiModal
+			open={showModal}
+			onClose={handleClose}
+			className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+		>
 			<>
 				<button
 					className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
@@ -37,7 +65,73 @@ function Modal() {
 					<XIcon className="h-6 w-6" />
 				</button>
 
-				<div></div>
+				<div className="relative  pt-[56.25%]">
+					<ReactPlayer
+						url={`https://www.youtube.com/watch?v=${trailer}`}
+						width="100%"
+						height="100%"
+						style={{ position: 'absolute', top: '0', left: '0' }}
+						playing
+						muted={muted}
+					/>
+					<div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+						<div className="flex space-x-2">
+							<button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+								<FaPlay className="h-7 w-7 text-black" />
+								Reproducir
+							</button>
+
+							<button className="modalButton">
+								<PlusIcon className="h-7 w-7" />
+							</button>
+
+							<button className="modalButton">
+								<ThumbUpIcon className="h-7 w-7" />
+							</button>
+						</div>
+						<button className="modalButton" onClick={() => setMuted(!muted)}>
+							{muted ? (
+								<VolumeOffIcon className="h-6 w-6" />
+							) : (
+								<VolumeUpIcon className="h-6 w-6" />
+							)}
+						</button>
+					</div>
+				</div>
+				<div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+					<div className="space-y-6 text-lg">
+						<div className="flex items-center space-x-2 text-sm">
+							<p className="font-semibold text-green-400">
+								{pelicula?.vote_average * 10}% de coincidencia
+							</p>
+							<p className="font-light">
+								{pelicula?.release_date || pelicula?.first_air_date}
+							</p>
+							<div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+								HD
+							</div>
+						</div>
+						<div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+							<p className="w-5/6">{pelicula?.overview}</p>
+							<div className="flex flex-col space-y-3 text-sm">
+								<div>
+									<span className="text-[gray]">Géneros:</span>{' '}
+									{genres.map((genre) => genre.name).join(', ')}
+								</div>
+
+								<div>
+									<span className="text-[gray]">Idioma Original:</span>{' '}
+									{pelicula?.original_language}
+								</div>
+
+								<div>
+									<span className="text-[gray]">Votos totales:</span>{' '}
+									{pelicula?.vote_count}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</>
 		</MuiModal>
 	);
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { useRecoilState } from 'recoil';
+import { modalState, movieState } from '../atoms/modalAtom';
 import { Pelicula } from '../typings';
 
 interface Props {
@@ -8,9 +10,16 @@ interface Props {
 }
 
 function Thumbnail({ pelicula }: Props) {
+	const [showModal, setShowModal] = useRecoilState(modalState);
+	const [peliculaActual, setCurrentMovie] = useRecoilState(movieState);
+
 	return (
 		<div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
 			<Image
+				onClick={() => {
+					setCurrentMovie(pelicula);
+					setShowModal(true);
+				}}
 				src={`https://image.tmdb.org/t/p/w500${
 					pelicula.backdrop_path || pelicula.poster_path
 				}`}
