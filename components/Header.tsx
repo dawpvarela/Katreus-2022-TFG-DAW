f3c73e16d4059978671bcea2b9427ad88f082d0c
pablo <varelapablo2002@hotmail.com>
2022-11-02 21:11:29 +0100

    Habilitar Autenticacion Usuario/Contraseña Firebase- 02/11/2022 21:11

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,11 @@ import { BellIcon, SearchIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import useAuth from '../hooks/useAuth';
 
 function Header() {
 	const [isScrolled, setIsScrolled] = useState(false);
+	const { logout } = useAuth();
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -47,13 +49,14 @@ function Header() {
 				<SearchIcon className="sm hidden h-6 w-6 sm:inline" />
 				<p className="hidden lg:inline">Infantil</p>
 				<BellIcon className="h-6 w-6" />
-				<Link href="/account">
-					<img
-						src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
-						alt=""
-						className="cursor-pointer rounded"
-					/>
-				</Link>
+				{/* <Link href="/account"> */}
+				<img
+					onClick={logout}
+					src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+					alt=""
+					className="cursor-pointer rounded"
+				/>
+				{/* </Link> */}
 			</div>
 		</header>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Row from '../components/Row';
+import useAuth from '../hooks/useAuth';
 import { Pelicula } from '../typings';
 import requests from '../utils/requests';
 
@@ -26,6 +27,10 @@ const Home = ({
 	peliculasRomanticas,
 	documentales,
 }: Props) => {
+	const { logout, loading } = useAuth();
+
+	if (loading) return null;
+
 	return (
 		<div
 			className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] `}
